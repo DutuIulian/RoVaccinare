b0f2acf7cadc546d187714a1fb48b47d58a011a5
Dutu Iulian <dutu.iulian98@gmail.com>
2021-05-03 01:26:28 +0300

    Added form validation logic

--- frontend/src/Register.js
+++ frontend/src/Register.js
@@ -1,64 +1,105 @@
+import React from 'react';
 import './register.scss';
 
-function Register() {
-	return (
-		<div class="form_wrapper">
-			<div class="form_container">
-				<div class="title_container">
-					<h2>Înregistrare utilizator</h2>
-				</div>
-				<div class="clearfix">
-					<div class="">
-						<form>
-							<div class="input_field">
-								<span>
-									<i aria-hidden="true" class="fa fa-envelope"/>
-								</span>
-								<input type="email" name="email" placeholder="E-mail" required />
-							</div>
-							<div class="input_field">
-								<span>
-									<i aria-hidden="true" class="fa fa-lock"/>
-								</span>
-								<input type="password" name="password" placeholder="Parola" required />
-							</div>
-							<div class="input_field">
-								<span>
-									<i aria-hidden="true" class="fa fa-lock"/>
-								</span>
-								<input type="password" name="password" placeholder="Parola (confirmare)" required />
-							</div>
-							<div class="input_field">
-								<span>
-									<i aria-hidden="true" class="fa fa-user"/>
-								</span>
-								<input type="text" name="name" placeholder="Nume"/>
-							</div>
-							<div class="input_field">
-								<span>
-									<i aria-hidden="true" class="fa fa-user"/>
-								</span>
-								<input type="text" name="name" placeholder="Prenume" required />
-							</div>
-							<div class="input_field">
-								<span>
-									<i aria-hidden="true" class="fa fa-address-card"/>
-								</span>
-								<input type="text" name="cnp" placeholder="CNP" required />
-							</div>
-							<div class="input_field">
-								<span>
-									<i aria-hidden="true" class="fa fa-address-card"/>
-								</span>
-								<input type="text" name="address" placeholder="Adresă" required />
-							</div>
-							<input class="button" type="submit" value="Înregistrare"/>
-						</form>
+class Register extends React.Component {
+	
+	constructor(props){
+		super(props);
+
+		this.state = {
+			fields: {},
+			errors: {}
+		}
+	}
+	
+	handleValidation(){
+		let fields = this.state.fields;
+		let errors = {};
+
+		if(fields["password"].localeCompare(fields["confirmPassword"]) !== 0) {
+			errors["password"] = "Parolele diferă";
+			return false;
+		}
+		
+		this.setState({errors: errors});
+		return true;
+	}
+	
+	formSubmit(e){
+		e.preventDefault();
+
+		if(this.handleValidation()) {
+			
+		}
+	}
+	
+	handleChange(field, e){			
+		let fields = this.state.fields;
+		fields[field] = e.target.value;		  
+		this.setState({fields});
+	}
+	
+	render() {
+		return (
+			<div class="form_wrapper">
+				<div class="form_container">
+					<div class="title_container">
+						<h2>Înregistrare utilizator</h2>
+					</div>
+					<div class="clearfix">
+						<div class="">
+							<form onSubmit={this.formSubmit.bind(this)}>
+								<div class="input_field">
+									<span>
+										<i aria-hidden="true" class="fa fa-envelope"/>
+									</span>
+									<input type="email" name="email" placeholder="E-mail" onChange={this.handleChange.bind(this, "email")} required />
+								</div>
+								<div class="input_field">
+									<span>
+										<i aria-hidden="true" class="fa fa-lock"/>
+									</span>
+									<input type="password" name="password" placeholder="Parola" onChange={this.handleChange.bind(this, "password")} required />
+								</div>
+								<div class="input_field">
+									<span>
+										<i aria-hidden="true" class="fa fa-lock"/>
+									</span>
+									<input type="password" name="confirmPassword" placeholder="Parola (confirmare)" onChange={this.handleChange.bind(this, "confirmPassword")} required />
+								</div>
+								<div class="input_field">
+									<span>
+										<i aria-hidden="true" class="fa fa-user"/>
+									</span>
+									<input type="text" name="lastName" placeholder="Nume" onChange={this.handleChange.bind(this, "lastName")} required />
+								</div>
+								<div class="input_field">
+									<span>
+										<i aria-hidden="true" class="fa fa-user"/>
+									</span>
+									<input type="text" name="firstName" placeholder="Prenume" onChange={this.handleChange.bind(this, "firstName")} required />
+								</div>
+								<div class="input_field">
+									<span>
+										<i aria-hidden="true" class="fa fa-address-card"/>
+									</span>
+									<input type="text" name="cnp" pattern="[0-9]*" placeholder="CNP" onChange={this.handleChange.bind(this, "cnp")} required />
+								</div>
+								<div class="input_field">
+									<span>
+										<i aria-hidden="true" class="fa fa-address-card"/>
+									</span>
+									<input type="text" name="address" placeholder="Adresă" requiredonChange={this.handleChange.bind(this, "address")} required />
+								</div>
+								<input class="button" type="submit" value="Înregistrare"/>
+								<span style={{color: "red", "display": "table", "margin": "0 auto"}}>{this.state.errors["password"]}</span>
+							</form>
+						</div>
 					</div>
 				</div>
 			</div>
-		</div>
-	);
+		);
+	}
 }
 
 export default Register;
